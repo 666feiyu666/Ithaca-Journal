b8881e5cf5e4c903d1f41d04b0d2c0842d8b4828
Frank <feiyut666@gmail.com>
2026-08-21 16:17:01 +0800

    0.2.0 developed

--- migrations/0002_access_identity.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE users ADD COLUMN access_subject TEXT;
-
-CREATE UNIQUE INDEX users_access_subject_idx
-  ON users(access_subject)
-  WHERE access_subject IS NOT NULL;
-
-DROP TABLE sessions;
-DROP TABLE invites;
